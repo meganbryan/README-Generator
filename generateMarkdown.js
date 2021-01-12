f4a0ab39251b2f8ad5a26461da2759a15eee8d60
Megan Bryan <meganmbryan12@gmail.com>
2021-01-12 13:35:25 -0800

    Add prompts and ability to produce README file with some initial styling

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ###   ${data.description}
+  
+  ## Table of Contents
+    1. [Installation] (#installation)
+    2. [Usage] (#usage)
+    3. [License] (#license)
+    4. [Contributing] (#contributing)
+    5. [Tests] (#tests)
+    6. [Questions] (#questions)
+    7. [Link to Application] (#link)
+    8. [Screenshot of Application] (#screenshot)
 
+  ## Installation <a name="installation"></a>
+  ### ${data.installation}
+
+  ## Usage <a name="usage"></a>
+  ### ${data.usage}
+
+  ## License <a name="license"></a>
+  ### ${data.License}
+
+  ## Contributing <a name="contributing"></a>
+  ### ${data.contributing}
+
+  ## Tests <a name="tests"></a>
+  ### ${data.tests}
+
+  ## Questions <a name="questions"></a>
+  ### ${data.questions}
+
+  ## Link to Deployed Application: 
+  ### [${data.URLName}](${data.URL}) <a name="link"></a>
+  ## ![${data.altText}](${data.photoFile}) <a name="screenshot"></a>
 `;
 }
 
--- index.js
+++ index.js
@@ -1,15 +1,93 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
+
 // array of questions for user
 const questions = [
-
+    { 
+        type: 'input',
+        message: 'What is the title of your README?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Write a brief description of your project:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Usage Instructions:',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        choices: ["BSD", "MIT", "GPL"],
+        name: 'License'
+    },
+    {
+        type: 'input',
+        message: 'List your contributors:',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'List your tests:',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Where can you be contacted with any questions?',
+        name: 'questions'
+    },
+    {
+        type: 'input',
+        message: 'Provide the URL for your deployed application:',
+        name: 'URL'
+    },
+    {
+        type: 'input',
+        message: 'Give a display name for this URL:',
+        name: 'URLName'
+    },
+    {
+        type: 'input',
+        message: 'Add a screenshot of your application:',
+        name: 'photoFile'
+    },
+    {
+        type: 'input',
+        message: 'Give Alt Text for your Screenshot:',
+        name: 'altText'
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.error(err) : console.log('File written successfully!')
+    )
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const markdownText = generateMarkdown(answers)
+        writeToFile("README.md", markdownText);
+    })
+    .catch(error => {
+        if(error) {
+            console.error(error)
+        } else {
+            console.log('Commit logged!')
+        }
+    })
 }
 
 // function call to initialize program
